3fc4169e0d4b324ad26ca5cfbd2f033073de126f
BovbelVladislav <bovbelvladislav@gmail.com>
2025-11-25 21:21:29 +0300

    Удалил Class1 Добавил новый Exam с конструкторами м методом ToString()

---
+++ Laba 10/Exam.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Laba_10
+{
+    internal class Exam
+    {
+        string Distipline { get; set; }
+        int Grade { get; set; }
+        DateTime Date_exam { get; set; }
+        public Exam(string distipline, int grade, DateTime dete_exam)
+        {
+            Distipline=distipline;
+            Grade=grade;
+            Date_exam=dete_exam;
+        }
+        public Exam()
+        {
+            Distipline = "Арифм.лог.";
+            Grade = 0;
+            Date_exam = new DateTime(2025, 1, 15, 8, 0, 0);
+        }
+        public override string ToString()
+        {
+            return $"${Distipline}, оценка: {Grade}, дата экзамена: {Date_exam}";
+        }
+    }
+}
