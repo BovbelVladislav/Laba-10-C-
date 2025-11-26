4f0c1061892660419796b46fade91ca9f2f05365
BovbelVladislav <bovbelvladislav@gmail.com>
2025-11-26 10:32:30 +0300

    Доделал Exam

--- Laba 10/Exam.cs
+++ Laba 10/Exam.cs
@@ -2,29 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 using static System.Net.Mime.MediaTypeNames;
-
+//Лиза
 namespace Laba_10
 {
     internal class Exam
     {
         string Distipline { get; set; }
-        int Grade { get; set; }
+        int grade;
+        public int Grade { get; set; }
         DateTime Date_exam { get; set; }
         public Exam(string distipline, int grade, DateTime dete_exam)
         {
             Distipline=distipline;
-            Grade=grade;
+            this.grade=grade;
             Date_exam=dete_exam;
         }
         public Exam()
         {
             Distipline = "Арифм.лог.";
-            Grade = 0;
+            grade = 0;
             Date_exam = new DateTime(2025, 1, 15, 8, 0, 0);
         }
         public override string ToString()
         {
-            return $"${Distipline}, оценка: {Grade}, дата экзамена: {Date_exam}";
+            return $"${Distipline}, оценка: {grade}, дата экзамена: {Date_exam}";
         }
     }
 }
--- Laba 10/Program.cs
+++ Laba 10/Program.cs
@@ -4,3 +4,4 @@
     Вachelor,
     SecondEducation
 }
+//Влад
