f3c20004d889cb55d44a3de70afc6fd4e919d210
BovbelVladislav <bovbelvladislav@gmail.com>
2025-11-26 10:48:27 +0300

    Убрал лишнее

--- Laba 10/Exam.cs
+++ Laba 10/Exam.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using static System.Net.Mime.MediaTypeNames;
-//Лиза
+
 namespace Laba_10
 {
     internal class Exam
--- Laba 10/Program.cs
+++ Laba 10/Program.cs
@@ -4,4 +4,3 @@
     Вachelor,
     SecondEducation
 }
-//Влад
