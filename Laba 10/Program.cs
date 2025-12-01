dba789e226f24ab54e120b412113e2c912f755e2
KozhanLiza <lizakozhan@gmail.com>
2025-12-01 08:19:08 +0300

    лишнее

--- Laba 10/Program.cs
+++ Laba 10/Program.cs
@@ -34,23 +34,6 @@ namespace Laba_10
                 new Exam("JS",0,DateTime.Now.AddMonths(-1))
             });
             Console.WriteLine(student.ToString());
-            const int rows = 1000;
-            const int cols = 1000;
-            const int total = rows * cols;
-            Exam[] oneDim = new Exam[total];
-            for (int i = 0; i < total; i++)
-                oneDim[i] = new Exam("Math", 5, DateTime.Now);
-            Exam[,] twoDimRect = new Exam[rows, cols];
-            for (int i = 0; i < rows; i++)
-                for (int j = 0; j < cols; j++)
-                    twoDimRect[i, j] = new Exam("Math", 5, DateTime.Now);
-            Exam[][] twoDimJagged = new Exam[rows][];
-            for (int i = 0; i < rows; i++)
-            {
-                twoDimJagged[i] = new Exam[cols];
-                for (int j = 0; j < cols; j++)
-                    twoDimJagged[i][j] = new Exam("Math", 5, DateTime.Now);
-            }
         }
     }
 }
